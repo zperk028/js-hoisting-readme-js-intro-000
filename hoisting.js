b5625bb4783cb0b557e66c6d8914b2fa938fe282
Zachary Perkins <ezmac028@gmail.com>
2020-10-05 22:36:17 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,11 @@
+var lyric = "maybe"
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
@@ -12,8 +13,8 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+  function thisIsCrazy(){
+    console.log("hey!!!");
   }
 }
 
@@ -24,7 +25,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
