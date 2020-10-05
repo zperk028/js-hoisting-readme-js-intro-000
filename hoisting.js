92bef85d02e8c53c73e9b7b21b90e41421893d17
Zachary Perkins <ezmac028@gmail.com>
2020-10-05 22:16:15 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,11 @@
+var lyric = "maybe"
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
